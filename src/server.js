c99b5b62af10bbb829abc48536c787c13eb44edb
mocakosan@naver.com <mocakosan@naver.com>
2022-08-16 19:14:50 +0900

    Development Environment

--- src/server.js
+++ src/server.js
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+//pug를 view enging으로 설정
+app.set("view engine","pug");
+//express에 template 위치 지정
+app.set("views", __dirname + "/views");
+//public url을 생성해서 유저에게 파일 공유
+app.use("/public", express.static(__dirname + "/public"));
+// home.pug를 render해주는 route handle
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
